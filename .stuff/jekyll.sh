8dbf9a58bca13430cc897c0a9ff3b203689753a5
jaiverma <jai2.verma@outlook.com>
2021-05-29 19:21:56 +0530

    Fixing Jekyll version in docker start script

--- .stuff/jekyll.sh
+++ .stuff/jekyll.sh
@@ -1,8 +1,15 @@
 #!/bin/zsh
 
-export JEKYLL_VERSION=latest
+export JEKYLL_VERSION=4.1.0
 docker run --rm \
     --volume="`pwd`:/srv/jekyll" \
     -p 4000:4000 \
     -it jekyll/jekyll:$JEKYLL_VERSION \
     bash
+
+
+# modify `config.yml` and set local theme and comment out `remote_theme` when
+# testing locally
+
+# run `bundle install` followed by `bundle exec jekyll server --host 0.0.0.0`
+# to test locally
